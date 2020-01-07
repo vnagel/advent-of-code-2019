d9998b80cde9432b328dc540ba4d901771102b32
Vincent Nagel <vnagel@umich.edu>
2020-01-06 19:54:20 -0500

    finished day 3 part 2

--- advent-of-code-2019-kotlin/src/main/kotlin/days/Day03.kt
+++ advent-of-code-2019-kotlin/src/main/kotlin/days/Day03.kt
@@ -8,39 +8,56 @@ class Day03(inputString: String) : Day(inputString) {
 
     override fun getPartOneSolution(): Any {
         val wirePaths: List<List<Point>> = inputList.map(::createWirePath)
-        val wirePathsNoOrigin = wirePaths.map{it.drop(1)}
+        val wirePathsNoOrigin = wirePaths.map { it.drop(1) }
         val intersections = getIntersections(wirePathsNoOrigin)
-        val distancesToOrigin = intersections.map {it.distanceTo(Point.origin)}
+        val distancesToOrigin = intersections.map { it.distanceTo(Point.origin) }
         val minDistanceToOrigin = distancesToOrigin.min()
         return minDistanceToOrigin ?: throw Exception("No min distance to origin")
     }
 
     override fun getPartTwoSolution(): Any {
-        return ""
+        val wirePaths: List<List<Point>> = inputList.map(::createWirePath)
+        val wirePathsNoOrigin = wirePaths.map { it.drop(1) }
+        val intersections = getIntersections(wirePathsNoOrigin).toSet()
+        val stepsToIntersectionsAllPaths = wirePaths.map { path ->
+            path.mapIndexed { step, pt -> Pair(step, pt) }.filter { intersections.contains(it.second) }
+                .distinctBy { it.second }.sortedBy { it.second }.map { it.first }
+        }
+        val combinedSteps = (0 until stepsToIntersectionsAllPaths[0].size).map { intersection ->
+            stepsToIntersectionsAllPaths.sumBy { it[intersection] }
+        }
+        val minCombinedSteps = combinedSteps.min()
+        return minCombinedSteps ?: throw Exception("No min combined steps")
     }
 
-    data class Point(val x: Int, val y: Int) {
+    data class Point(val x: Int, val y: Int) : Comparable<Point> {
         companion object {
-            val origin = Point(0,0)
+            val origin = Point(0, 0)
         }
+
         // Returns list of points in interval (currentLocation, currentLocation + direction * magnitude]
-        fun getMovementPath(direction: Char, magnitude: Int) : List<Point>{
-            return when(direction){
-                'U' -> (1..magnitude).map{Point(x, y + it)}
-                'D' -> (1..magnitude).map{Point(x, y - it)}
-                'R' -> (1..magnitude).map{Point(x + it, y)}
-                'L' -> (1..magnitude).map{Point(x - it, y)}
+        fun getMovementPath(direction: Char, magnitude: Int): List<Point> {
+            return when (direction) {
+                'U' -> (1..magnitude).map { Point(x, y + it) }
+                'D' -> (1..magnitude).map { Point(x, y - it) }
+                'R' -> (1..magnitude).map { Point(x + it, y) }
+                'L' -> (1..magnitude).map { Point(x - it, y) }
                 else -> throw Exception("Unknown direction")
             }
         }
-        fun distanceTo(other: Point): Int{
+
+        fun distanceTo(other: Point): Int {
             return abs(x - other.x) + abs(y - other.y)
+        }
+
+        override fun compareTo(other: Point): Int {
+            return (this.x + this.y).compareTo(other.x + other.y)
         }
     }
 
-    fun createWirePath(inputPath:String) : List<Point> {
+    fun createWirePath(inputPath: String): List<Point> {
         val wirePath: MutableList<Point> = arrayListOf(Point.origin)
-        for(move in inputPath.split(",")){
+        for (move in inputPath.split(",")) {
             val direction = move[0]
             val magnitude = move.substring(1).toInt()
             val movementPath = wirePath.last().getMovementPath(direction, magnitude)
@@ -49,10 +66,10 @@ class Day03(inputString: String) : Day(inputString) {
         return wirePath
     }
 
-    fun getIntersections(wirePaths: List<List<Point>>) : List<Point>{
+    fun getIntersections(wirePaths: List<List<Point>>): List<Point> {
         val firstPathSet = wirePaths[0].toSet()
         val intersections: MutableList<Point> = ArrayList()
-        for(index in 1 until wirePaths.size){
+        for (index in 1 until wirePaths.size) {
             val currentIntersections = wirePaths[index].filter { firstPathSet.contains(it) }
             intersections.addAll(currentIntersections)
         }
--- advent-of-code-2019-kotlin/src/test/kotlin/days/Day03Test.kt
+++ advent-of-code-2019-kotlin/src/test/kotlin/days/Day03Test.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 class Day03Test {
     @Test
-    fun `input 1 part 1`() {
+    fun `part 1 input 1`() {
         val input = "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" +
                 "U62,R66,U55,R34,D71,R55,D58,R83"
         val day03 = Day03(input)
@@ -15,12 +15,32 @@ class Day03Test {
     }
 
     @Test
-    fun `input 2 part 1`() {
+    fun `part 1 input 2`() {
         val input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" +
                 "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
         val day03 = Day03(input)
         val actual = day03.getPartOneSolution()
         val expected = 135
+        Truth.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `part 2 input 1`() {
+        val input = "R75,D30,R83,U83,L12,D49,R71,U7,L72\n" +
+                "U62,R66,U55,R34,D71,R55,D58,R83"
+        val day03 = Day03(input)
+        val actual = day03.getPartTwoSolution()
+        val expected = 610
+        Truth.assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun `part 2 input 2`() {
+        val input = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\n" +
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+        val day03 = Day03(input)
+        val actual = day03.getPartTwoSolution()
+        val expected = 410
         Truth.assertThat(actual).isEqualTo(expected)
     }
 
